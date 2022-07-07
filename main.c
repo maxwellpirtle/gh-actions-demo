3ef95a8b25ae1b5c822df55c983f8620562292f1
Maxwell Pirtle <maxwellpirtle@me.com>
2022-07-06 22:04:36 -0400

    Add pre-commit options

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int
-main(int argc, const char **argv)
-{
+int main(int argc, const char **argv) {
   printf("This is a test for CMake actions!\n");
   return 0;
 }
